1758da0199549309d93c85c82d9b58fd2ebeaaaa
CaptainCollie <shadaraevd@gmail.com>
2020-12-26 11:37:54 +0800

    new

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,141 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+    Util util = new Util();
 
+    public void createUsersTable() {
+        Connection connection = util.connect();
+        Statement statement = null;
+        String sql = "create table if not exists UsersData (id mediumint primary key not null AUTO_INCREMENT, name CHAR(30) not null, lastName char(30) not null, age int not null);";
+
+        try {
+            statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+        }
     }
 
     public void dropUsersTable() {
+        Connection connection = util.connect();
+        Statement statement = null;
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        String sql = "drop table if exists UsersData;";
 
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        Connection connection = util.connect();
+        Statement statement = null;
+        String sql = "insert into UsersData (name, lastName, age) values ('" + name + "', '" + lastName + "', " + age + ");";
 
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void removeUserById(long id) {
+        Connection connection = util.connect();
+        Statement statement = null;
+        String sql = "delete  from UsersData where id=" + id + ";";
 
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        Connection connection = util.connect();
+        PreparedStatement preparedStatement = null;
+        List<User> allUsers = new LinkedList<>();
+        String sql = "select * from UsersData";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                long i = resultSet.getLong(1);
+                User user = new User(resultSet.getString(2), resultSet.getString(3), resultSet.getByte(4));
+                user.setId(i);
+                allUsers.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return allUsers;
     }
 
     public void cleanUsersTable() {
-
+        Connection connection = util.connect();
+        Statement statement = null;
+        String sql = "truncate table UsersData;";
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:mysql://localhost:3306/test?useSSL=false&useUnicode=true&serverTimezone=UTC";
+    private static final String username = "root";
+    private static final String password = "100799";
+
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
