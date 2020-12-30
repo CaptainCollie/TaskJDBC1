d18b9147dc58b5a6ca2cc9f8200144a30ef3ca53
CaptainCollie <shadaraevd@gmail.com>
2020-12-30 13:32:35 +0800

    new

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,15 +1,14 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Hibernate;
-import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Vasya", "Ivanov", (byte) 30);
         System.out.println("User с именем – Vasya добавлен в базу данных");
